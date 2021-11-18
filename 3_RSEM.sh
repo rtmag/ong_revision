c394fc9a36ae0ee5e67327b4757cc82ee426d030
Roberto Tirado Magallanes <rtmag4@gmail.com>
2021-11-19 07:40:38 +0800

    Update 3_RSEM.sh

--- 3_RSEM.sh
+++ 3_RSEM.sh
@@ -7,13 +7,13 @@
 ################################################################################################################################################
 #  gen the commands
 ls -1 STAR/*bam | perl -pe 's/^STAR\/(.+)_Aligned.toTranscriptome.out.bam/\~\/myPrograms\/RSEM\-1\.3\.0\/rsem\-calculate\-expression \\\
-    \-p 50 \-\-paired\-end \-\-bam \-\-estimate\-rspd \-\-output\-genome\-bam \\\
+    \-p 5 \-\-paired\-end \-\-bam \-\-estimate\-rspd \-\-output\-genome\-bam \\\
     \.\/STAR\/$1_Aligned.toTranscriptome.out.bam \\\
     \~\/resources\/RSEM\_REF\_GRCh38\/GRCh38 \\\
-    RSEM\/RSEM\_$1\n/g' > RSEM.sh
+    RSEM\/RSEM\_$1 &\n/g' > RSEM.sh
 
 # run it
-bash STAR.sh
+bash RSEM.sh
 #################################################################################################################################################
 
 # comment the first one
